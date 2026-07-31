0907a57f5737c4b8900806afcab20ce09009b071
Cardibro <159860928+Cardibro@users.noreply.github.com>
2026-07-31 18:04:43 -0500

    installing packages

--- pkg/main.py
+++ pkg/main.py
@@ -1,8 +1,14 @@
 import sys
 
-def run(attributes, flags, main):
-    if attributes.install != None:
+def run(attributes, flags):
+    if attributes.get("install") is not None:
         main_module = sys.modules['__main__']
-        main_module.install_package(attributes.install)
-    else:
-        print("Please use `pkg help` for help on installing packages")
+        main_module.install_package(attributes.get("install"))
+        return True
+
+    if attributes.get("remove") is not None:
+        main_module = sys.modules['__main__']
+        main_module.remove_package(attributes.get("remove"))
+        return True
+
+    print("Please use `pkg help` for help with packages")
