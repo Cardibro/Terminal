9d228cf98139a6ecfd273ae215510639328e14c4
Cardibro <159860928+Cardibro@users.noreply.github.com>
2026-07-31 17:42:02 -0500

    Initial Commit

--- pkg/main.py
+++ pkg/main.py
@@ -3,6 +3,6 @@ import sys
 def run(attributes, flags, main):
     if attributes.install != None:
         main_module = sys.modules['__main__']
-        main_module.install_package()
+        main_module.install_package(attributes.install)
     else:
         print("Please use `pkg help` for help on installing packages")
